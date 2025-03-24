89887b0b179b3a77160957cdd83c8b42d7c25967
Dieter Frehlich <64315649+frehlid@users.noreply.github.com>
2025-03-24 10:44:41 -0700

    obstacle spawner code

---
+++ ECS_DRAFT/src/systems/ai/obstacle_spawner/obstacle_spawner.h
@@ -0,0 +1,8 @@
+#include "../../../common.hpp"
+#include "../../../tinyECS/component_container.hpp"
+#include "../../../tinyECS/components.hpp"
+#include "../../../tinyECS/registry.hpp"
+#include "systems/ISystem.hpp"
+#include "../../world/world_init.hpp"
+
+void obstacle_spawner_step(float elapsed_ms);
